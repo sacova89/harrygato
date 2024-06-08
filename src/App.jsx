8f546d759ddc6c754dc85d119936dccd883abc7b
David Santacruz R <axelsantacruzr@gmail.com>
2024-06-07 19:16:44 -0500

    feat: initial harry el sucio gato project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className={"text-black text-3xl font-bold underline"}>
+        HELLO FUCKING WORLD
+        https://clancy.netlify.app/
+    </div>
+  )
+}
+
+export default App
