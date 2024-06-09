66594bdf4eb36f6577f584a3bfd035743d815256
David Santacruz R <axelsantacruzr@gmail.com>
2024-06-08 20:53:37 -0500

    fix: remove not needed routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {About} from "./components/about.jsx";
-import {Routes, Route} from "react-router-dom";
 import {Footer} from "./components/footer.jsx";
 import {Portfolio} from "./components/portfolio.jsx";
 import {Navbar} from "./components/navbar.jsx";
